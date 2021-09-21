9d25bf37afbb2a464562cfb1a761469efd53e12c
pre-commit <pre-commit@example.com>
2021-09-21 19:00:53 +0000

    pre-commit fixes

--- code/main.tf
+++ code/main.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version  = ">= 0.12.0"
+  required_version = ">= 0.12.0"
 }
 
 provider "azurerm" {
-  subscription_id   = var.subscription
-  version       = ">= 2.17"
-  features      {}
+  subscription_id = var.subscription
+  version         = ">= 2.17"
+  features {}
 }
 
 data "azurerm_client_config" "current" {}
 
 
 resource "azurerm_resource_group" "aro_vnet_resource_group" {
-  name          = var.aro_vnet_resource_group_name
-  location      = var.aro_location
+  name     = var.aro_vnet_resource_group_name
+  location = var.aro_location
 
-  tags          = var.tags
+  tags = var.tags
 }
 
--- code/variables.tf
+++ code/variables.tf
@@ -1,18 +1,18 @@
 variable "subscription" {}
 
 variable "aro_vnet_resource_group_name" {
-  description 		= "Name of resource group to deploy ARO VNET/Subnets in."
+  description = "Name of resource group to deploy ARO VNET/Subnets in."
 }
 
 variable "aro_location" {
-  description 		= "The ARO location where all resources should be created"
+  description = "The ARO location where all resources should be created"
 }
 
 variable "tags" {
-  description 		= "Tags to apply to all resources created."
-  type        		= map(string)
-  default     		= {
-    Environment 	= "Production"
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default = {
+    Environment = "Production"
   }
 }
 
