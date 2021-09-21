3c9a2ea4db729c3d35d0afd9f0cea34394e45464
Yerzhan Beisembayev <ybeisemb@redhat.com>
2021-09-21 14:59:46 -0400

    Test37

--- code/main.tf
+++ code/main.tf
@@ -1,8 +1,20 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0.3"
-    }
-  }
+  required_version  = ">= 0.12.0"
 }
+
+provider "azurerm" {
+  subscription_id   = var.subscription
+  version       = ">= 2.17"
+  features      {}
+}
+
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_resource_group" "aro_vnet_resource_group" {
+  name          = var.aro_vnet_resource_group_name
+  location      = var.aro_location
+
+  tags          = var.tags
+}
+
