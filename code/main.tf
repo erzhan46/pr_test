8b91557c3f5ca130ab5d18b53420153824546f57
pre-commit <pre-commit@example.com>
2021-09-25 23:22:57 +0000

    pre-commit fixes

--- code/main.tf
+++ code/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.14.6"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">=2.20.0"
     }
   }
